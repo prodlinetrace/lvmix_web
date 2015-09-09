6ffda71e0fd2b8d5247666e1ed5cde2eb44c7f87
Piotr Wilkosz <Piotr.Wilkosz@gmail.com>
2015-09-10 01:24:52 +0200

    minor change

--- app/operation_statuses/forms.py
+++ app/operation_statuses/forms.py
@@ -2,13 +2,15 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import Optional, Length, Required, InputRequired
 from flask.ext.babel import gettext, lazy_gettext
-
+from ..models import Unit
 
 #unit_list = Unit.query.order_by(Unit.id.desc())
+#print unit_list
 #TODO: replace this ugly hardcode by db entries
 unit_choices = [('0', 'None'), ('1', 'N'), ('2', 'Nm'), ('3', 'deg'), ('4', 'mm')]
 
 class Operation_StatusForm(Form):
+
     id = IntegerField(lazy_gettext('Id'), validators=[InputRequired()])
     name = StringField(lazy_gettext('Name'), validators=[Required(), Length(1, 50)])
     unit_id = SelectField(lazy_gettext('Unit'), choices=unit_choices)
