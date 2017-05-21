cc80f60b56031d51aec407e959444740671325da
Piotr Wilkosz <piotr.wilkosz@gmail.com>
2017-05-21 14:56:08 +0200

    mergre code from prod as of 21-05-2017

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app('default')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1', threaded=True)
